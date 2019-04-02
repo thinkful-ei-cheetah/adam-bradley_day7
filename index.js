d0f83adb0067e240c5b910d06bc3d0e8c89a09df
Brad Schilit <bradleyschilit@mac.com>
2019-04-02 14:50:13 -0400

    more added

--- index.js
+++ index.js
@@ -3,22 +3,35 @@
 function getDogImage(num) {
   fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => console.log(responseJson))
+    .then(showImageJson => displayResults(showImageJson));
 }
 
 function inputValue(){
-    $('form').submit(function (event){
-        event.preventDefault();
-        let newVal = $('#inputVal').val();
-        if (newVal <1 || newVal > 50){
-            throw new Error ('Enter Number from 1-50');
-        }
-        console.log(newVal);
-        getDogImage(newVal);
-        return newVal;
-    });
+  $('form').submit(function (event){
+    event.preventDefault();
+    let newVal = $('#inputVal').val();
+    if (newVal <1 || newVal > 50){
+      throw new Error ('Enter Number from 1-50');
+    }
+    console.log(newVal);
+    getDogImage(newVal);
+    return newVal;
+  });
 }
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
 $(function() {
   console.log('App loaded! Waiting for submit!');
   inputValue();
+  
 });
