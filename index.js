4ca95b25ab4828edaae3bd4afdb0a0ae923c56b6
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-04-02 13:28:45 -0500

    update inputValue

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 'use strict';
 
-function getDogImage() {
-  fetch(`https://dog.ceo/api/breeds/image/random`)
+function getDogImage(num) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
     .then(responseJson => console.log(responseJson));
 }
 
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getDogImage();
-  });
-}
 function inputValue(){
     $('form').submit(function (event){
         event.preventDefault();
-        const newVal = $('#inputVal').val();
+        let newVal = $('#inputVal').val();
+        if (newVal <1 || newVal > 50){
+            throw new Error ('Enter Number from 1-50');
+        }
         console.log(newVal);
+        getDogImage(newVal);
         return newVal;
     });
 }
-function oneToFifty(val){
-    if (val < 1 || val >50){
-        throw new Error ('Not 1-50');
-    }
-}
-
 $(function() {
   console.log('App loaded! Waiting for submit!');
-  watchForm();
   inputValue();
-//   oneToFifty();
 });
